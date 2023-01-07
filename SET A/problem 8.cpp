aba55b2b244a7a9830e162d6857777600826ef2d
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-07 05:16:42 -0800

    Update problem 8.cpp

--- SET A/problem 8.cpp
+++ SET A/problem 8.cpp
@@ -19,3 +19,33 @@ public:
        return ans;
     }
 };
+
+
+/*OPTIMISATION
+PSEUDO CODE
+1.since the array is in sorted order , hence the least element is in the leftmost and biggest no is in the right most
+since array may contain negative elements too,it means after squaring the most negative element will become
+the greatest element ,hence in the ans array it will be on the right most.
+
+TC:O(N)
+SC:O(1)
+*/
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+       vector<int>ans(nums.size(),0);
+       int low=0,high=nums.size()-1;
+       for(int i=nums.size()-1;i>=0;i--){
+       if(abs(nums[low])>abs(nums[high])){
+           ans[i]=nums[low]*nums[low];
+           low++;
+       }
+       else{
+          ans[i]=nums[high]*nums[high];
+           high--; 
+        }
+       }
+       return ans;
+    }
+};
