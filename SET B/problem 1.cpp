90bf642745c7c16309aa780227b5434047a5d4c1
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-14 07:53:33 -0800

    Create problem 1.cpp

---
+++ SET B/problem 1.cpp
@@ -0,0 +1,12 @@
+PROBLEM: https://www.geeksforgeeks.org/count-pairs-array-whose-sum-less-x/
+Given a sorted integer array and number x, the task is to count pairs in array whose sum is less than x.
+  
+/*
+naive solution
+1.run two for loops and find every possible pair....and if sum<x cnt++
+tc: o(n^2)
+sc:O(1)
+*/
+  
+  
+  
