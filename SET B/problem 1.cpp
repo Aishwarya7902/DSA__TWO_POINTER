ae21b22808829fc2c507e68f99dd0e25f475dc25
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-15 03:12:30 -0800

    Update problem 1.cpp

--- SET B/problem 1.cpp
+++ SET B/problem 1.cpp
@@ -8,5 +8,24 @@ tc: o(n^2)
 sc:O(1)
 */
   
+#include <bits/stdc++.h>
+using namespace std;
+int countPair(vector<int>&nums,int x){
+    int cnt=0;
+    int n=nums.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(nums[i]+nums[j]<x)cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    vector<int>nums({1, 2, 3, 4, 5, 6, 7, 8});
+	cout<<countPair(nums,7);
+	return 0;
+}
+  
   
   
