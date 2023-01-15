eea76e808fa63ae2e03b27044e78771c9a1c9d28
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-15 03:41:30 -0800

    Update problem 1.cpp

--- SET B/problem 1.cpp
+++ SET B/problem 1.cpp
@@ -27,5 +27,52 @@ int main() {
 	return 0;
 }
   
-  
+
+/*
+two pointer based approach
+tc:o(n)
+sc:o(1)
+pseudo code
+1) Initialize two variables l and r to find the candidate 
+   elements in the sorted array.
+       (a) l = 0
+       (b) r = n - 1
+2) Initialize : result = 0
+2) Loop while l < r.
+
+    // If current left and current
+    // right have sum smaller than x,
+    // the all elements from l+1 to r
+    // form a pair with current
+    (a) If (arr[l] + arr[r] < x)  
+          result = result + (r - l)  
+          l++;    
+   
+    (b) Else
+            r--;
+       
+3) Return result
+*/
+
+int countPair(vector<int>&nums,int x){
+    int cnt=0;
+    int n=nums.size();
+    int l=0,r=n-1;
+    while(l<r){
+    // If current left and current
+    // right have sum smaller than x,
+    // the all elements from l+1 to r
+    // form a pair with current
+        if(nums[l]+nums[r]<x){
+            cnt+=r-l; //r-(l+1) +1 since 0 based indexing
+            l++;
+        }
+        else{
+            r--;
+        }
+        
+    }
+    return cnt;
+}
+
   
