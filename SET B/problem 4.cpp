f423b27e5f5cc9d75061c9e5ac9eb2936b36d060
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-01-18 10:31:49 -0800

    Update problem 4.cpp

--- SET B/problem 4.cpp
+++ SET B/problem 4.cpp
@@ -28,3 +28,47 @@ int main() {
 	cout<<subarraySumLessThanK(nums,k);
 	return 0;
 }
+
+
+/*
+two pointer
+tc:o(n)
+sc:o(1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int countSubarraySumLessThanK(vector<int>&nums,int k){
+    int start=0,end=0;
+    int count=0,sum=nums[0];
+    int n=nums.size();
+    while(start<n && end<n){
+        //if sum is less than k 
+        //move end by one position
+        //update count and sum accordingly
+        if(sum<k){
+            end++;
+        }
+        if(end>=start)
+        count+=end-start;
+        //for last element
+        //end may become n
+        if(end<n){
+            sum+=nums[end];
+        }
+        // if sum is greater than or equal to k 
+        //subtract arr[start] from sum
+        //deacrease sliding window by moving start by one position
+        else{
+            sum-=nums[start];
+            start++;
+        }
+        
+    }
+    return count;
+}
+
+int main() {
+    vector<int>nums({2,5,6});
+	cout<<countSubarraySumLessThanK(nums,10)<<endl;
+	return 0;
+}
