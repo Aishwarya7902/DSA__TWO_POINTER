044038514352dff310fae636fd2226043a0b8a07
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-05-03 03:03:07 -0700

    Create two sum.cpp

---
+++ SESSION_1/two sum.cpp
@@ -0,0 +1,46 @@
+PROBLEM:https://leetcode.com/problems/two-sum/description/
+
+/*
+brute force
+use two for loops
+*/
+
+/*
+optmisation
+use hashing
+*/
+
+/*
+two pointer
+use a pair of vector to store number and its correponding element before sorting
+tc:o(n)
+sc:o(n)
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        vector<pair<int,int>>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int i=0,j=v.size()-1;
+        while(i<j){
+            if(v[i].first+v[j].first==target){
+                ans.push_back(v[i].second);
+                ans.push_back(v[j].second);
+                break;
+
+            }
+            else if(v[i].first+v[j].first>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+        
+    }
+};
