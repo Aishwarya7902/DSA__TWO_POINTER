80ff1c1760ebc4d57b3f6343aeaec67327c4e275
Aishwarya7902 <71867316+Aishwarya7902@users.noreply.github.com>
2023-05-22 07:34:52 -0700

    Create ques1.cpp

---
+++ SESSION_4/ques1.cpp
@@ -0,0 +1,25 @@
+Given an array of integers, our goal is to find the length of the largest subarray having the sum of its elements at most ‘k’ where k>0.
+
+NOTE : all elements are +ve
+
+/*
+two pointer
+tc:o(n)
+sc:o(1)
+*/
+
+  int sizeOfLargestSubarray(int arr[],int n,int k){
+    int i=0,j=0,res=0;
+    long sum=0;
+    while(i<n){
+        while(j<n && sum<=k){
+            sum+=arr[j];
+            j++;
+        }
+        res=max(res,j-i);
+        sum-=arr[i];
+        i++;
+    }
+    return res;
+
+}
